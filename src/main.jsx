483bc5fac653b677c1d88eede242254a9b958861
Tomas-Acevedo <tomasace2727@gmail.com>
2025-12-22 19:02:09 -0300

    fix: windows-compatible build + pwa build working

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
+import { registerSW } from 'virtual:pwa-register'; // ✅ Importar el registro
+
+// ✅ Esto registra el Service Worker y permite que la app funcione offline e instalable
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
